302df0eb38dabfc09fb402184ea95f90ba97a51d
kylp <kyunglip1314@163.com>
2025-03-11 22:25:14 +0800

    add：更新知识速记。

---
+++ src/samples/RefReactive.vue
@@ -0,0 +1,31 @@
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg=ref(0)
+msg.value=10
+console.log("msg：",msg)
+function increment(){
+    msg.value++
+}
+</script>
+
+<template>
+    <div class="content">
+        <p>Ref：{{ msg }}</p>
+        <!-- <button @click="msg++">Add</button> -->
+        <button @click="increment">Add</button>
+
+    </div>
+</template>
+    
+<style scoped>
+
+.content{
+    height: 100pt;
+    background-color: aqua;
+    border-radius: 8pt;
+    padding-left: 5pt;
+    padding-top: 5pt;
+}
+</style>
---
+++ src/samples/SampleMain.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import RefReactive from './RefReactive.vue';
+
+
+</script>
+<template>
+<h1>Vue Sample 入口</h1>
+<div>
+    <RefReactive/>
+</div>
+</template>
